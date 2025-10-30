c25de573ace4b8c083ded830ab0f5506f0f1a111
Mohammadi04 <147467016+Mohammadi04@users.noreply.github.com>
2025-10-30 14:03:35 -0400

    Add files via upload

---
+++ ios101-project5-tumblr-starter/ios101-project5-tumblr/TumblrCell.swift
@@ -0,0 +1,29 @@
+//
+//  TumblrCell.swift
+//  ios101-project5-tumblr
+//
+//  Created by Tanzim Islam on 10/30/25.
+//
+
+import UIKit
+
+class TumblrCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var summaryLabel: UILabel!
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
